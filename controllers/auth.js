42fd6e2e18b66c05b67b98419bd9e893aec4ee48
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-08-11 22:22:09 +0300

    update

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,46 +1,76 @@
-const {User} = require("../models/user");
-const bcrypt = require('bcrypt')
+const { User } = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const { SECRET_KEY } = process.env;
 
 const { HttpError, ctrlWrapper } = require("../helpers");
 
-const register = async(req, res) => {
-    const {email, password, subscription} = req.body;
-    const user = await User.findOne({email});
+const register = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
-    if(user) {
-        throw HttpError(409, "Email in use");
-    }
+  if (user) {
+    throw HttpError(409, "Email in use");
+  }
 
-    const hashPassword = await bcrypt.hash(password, 10);
+  const hashPassword = await bcrypt.hash(password, 10);
 
-    const newUser = await User.create({...req.body, password: hashPassword});
-    // res.status(201).json(newUser);
+  const newUser = await User.create({ ...req.body, password: hashPassword });
 
-    res.status(201).json({
-        password: newUser.password,
-        email: newUser.email,
-        subscription: newUser.subscription,
-    })
+  res.status(201).json({
+    user: {
+      email: newUser.email,
+      subscription: newUser.subscription,
+    },
+  });
 };
 
-const login = async(req, res) => {
-    const {email, password} = req.body;
-    const user = await User.findOne({email});
-    if(!user) {
-        throw HttpError(401, "Email or password is wrong");
-    }
-    const passwordCompare = await bcrypt.compare(password, user.password);
-    if(!passwordCompare) {
-        throw HttpError(401, "Email or password is wrong");
-    }
-    const token = "";
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare || !user) {
+    throw HttpError(401, "Email or password is wrong");
+  }
 
-    res.json({
-        token,
-    })
-}
+  const payload = {
+    id: user._id,
+  };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+
+  await User.findByIdAndUpdate(user._id, { token });
+
+  res.json({
+    token,
+    user: {
+      email: user.email,
+      subscription: user.subscription,
+    },
+  });
+};
+const getCurrent = async (req, res) => {
+  const { email, subscription } = req.user;
+
+  res.json({
+    email,
+    subscription,
+  });
+};
+
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
+
+  res.status(204).json();
+};
 
 module.exports = {
-    register: ctrlWrapper(register),
-    login: ctrlWrapper(login),
-}
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+  getCurrent: ctrlWrapper(getCurrent),
+  logout: ctrlWrapper(logout),
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,15 +1,22 @@
-const express = require('express');
+const express = require("express");
 const controll = require("../../controllers/auth");
 
 const router = express.Router();
 
-const {validateBody} = require('../../middlewapres');
+const { validateBody, authenticate } = require("../../middlewapres");
 
-const {schemas} = require('../../models/user');
+const { schemas } = require("../../models/user");
 
-router.post('./register', validateBody(schemas.registerShema), controll.register);
+router.post(
+  "/register",
+  validateBody(schemas.registerShema),
+  controll.register
+);
 
-router.post('./login', validateBody(schemas.loginShema), controll.login);
+router.post("/login", validateBody(schemas.loginShema), controll.login);
 
+router.get("/current", authenticate, controll.getCurrent);
+
+router.post("/logout", authenticate, controll.logout);
 
 module.exports = router;
